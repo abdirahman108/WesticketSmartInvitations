4f950a91b70ece36caaebe2a5b6a29f6e287fcc4
fadxiCabdi <47848451+fadxiCabdi@users.noreply.github.com>
2019-09-14 19:43:19 +0300

    Scanned Tickets results is working

--- app/src/main/java/com/westechhub/westicketsmartinvitations/check_list.java
+++ app/src/main/java/com/westechhub/westicketsmartinvitations/check_list.java
@@ -2,23 +2,40 @@ package com.westechhub.westicketsmartinvitations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.paperdb.Paper;
 
 public class check_list extends AppCompatActivity {
-    TextView txtTime, txtTicketNo;
+    TextView txtTime, ticketNo, chklist;
+
+    ListView listView;
+
+    private String  eventTicketNo="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
 
-        Paper.init(this );
-        txtTime = findViewById(R.id.timechk);
 
-        String timeID = "Time";
-        String showTime = Paper.book().read(timeID);
-        txtTime.setText("Scanned On: "+ showTime);
+
+
+        Paper.init(this );
+
+        List<String> allKeys = Paper.book().getAllKeys();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,allKeys);
+
+        listView = findViewById(R.id.chklist);
+
+        listView.setAdapter(adapter);
+
     }
 
 }
